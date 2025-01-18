aba1bdc4e995aca299b81f1cfa5ada2d422b7a80
eurizkd <lebgdu75dc@outlook.fr>
2025-01-18 22:26:00 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     bot: {
-        prefixe: '+', // Préfixe - Le texte avant les commandes
-        buyer: '1251209545129132110', // Buyer
+        prefixe: '-', // Préfixe - Le texte avant les commandes
+        buyer: '915321015515643935', // Buyer
         couleur: '#6495ED', // Couleur HEX
-        footer: '4Protect', // Dans la commande help ou autre il y'a un texte en bas
-        maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
+        footer: 'HEXAGONE', // Dans la commande help ou autre il y'a un texte en bas
+        maxServer: '3', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
